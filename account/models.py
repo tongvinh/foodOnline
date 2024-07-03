bb8ac62a7868ec0ddf9caf7b15818c75707fbc67
Vinh Tong <tonggiavinh2010@gmail.com>
2024-07-03 11:23:19 +0700

    Config custom User

--- account/models.py
+++ account/models.py
@@ -20,7 +20,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_super_user(self,first_name, last_name, username, email, password=None):
+    def create_superuser(self,first_name, last_name, username, email, password=None):
         user = self.create_user(
             email= self.normalize_email(email),
             username= username,
