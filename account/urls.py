75a6e80a33f70715cbd6e1ac39412f1c803995b0
Vinh Tong <tonggiavinh2010@gmail.com>
2024-07-08 11:30:48 +0700

    Vendor Register

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('registerUser/',registerUser, name='registerUser')
+    path('registerUser/',registerUser, name='registerUser'),
+    path('registerVendor/', registerVendor, name="registerVendor")
 ]
