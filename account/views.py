a0e7c22461e8c0f5eeaf1c19a4184e489f1a7510
Vinh Tong <tonggiavinh2010@gmail.com>
2024-07-12 10:10:42 +0700

    Load vendor profile image dynamically

--- account/views.py
+++ account/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
+from vendor.models import Vendor
 
 # Restrict the vendor form accessing the  customer page
 def check_role_vendor(user):
@@ -166,7 +167,11 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    return render(request, 'accounts/vendorDashboard.html')
+    vendor = Vendor.objects.get(user=request.user)
+    context = {
+        'vendor': vendor,
+    }
+    return render(request, 'accounts/vendorDashboard.html', context=context)
 
 def forgot_password(request):
     if request.method == 'POST':
