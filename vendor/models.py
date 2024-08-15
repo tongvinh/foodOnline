f755e386cdef880e84b0eca67dcc7e02a145d2a2
Vinh Tong <tonggiavinh2010@gmail.com>
2024-08-15 14:11:17 +0700

    Opening Hour Model

--- vendor/models.py
+++ vendor/models.py
@@ -1,3 +1,4 @@
+from datetime import time
 from django.db import models
 from account.models import User,UserProfile
 from account.utils import send_notification
@@ -36,3 +37,26 @@ class Vendor(models.Model):
                     send_notification(mail_subject, mail_template, context)
                     
         return super(Vendor, self).save(*args,**kwargs)
+    
+DAYS = [
+    (1, ('Monday')),
+    (2, ('Tuesday')),
+    (3, ('Wednesday')),
+    (4, ('Thursday')),
+    (5, ('Friday')),
+    (6, ('Saturday')),
+    (7, ('Sunday')),
+]
+
+HOURS_OF_DAY_24 = [(time(h, m).strftime('%I:%M %p'), time(h, m).strftime('%I:%M %p') )for h in range(0, 24) for m in (0, 30)]
+
+class OpeningHour(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    day = models.IntegerField(choices=DAYS)
+    from_hour = models.CharField(choices=HOURS_OF_DAY_24, max_length=10, blank=True)
+    to_hour = models.CharField(choices=HOURS_OF_DAY_24, max_length=10, blank=True)
+    is_closed = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ('day', 'from_hour')
+        unique_together = ('day', 'from_hour', 'to_hour')
