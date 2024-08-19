f01972ea51f1413883cfd9e36f6878be3c4bcde0
Vinh Tong <tonggiavinh2010@gmail.com>
2024-08-19 16:39:46 +0700

    Dynamic Tax Module

--- foodonline_main/static/js/custom.js
+++ foodonline_main/static/js/custom.js
@@ -92,7 +92,7 @@ $(document).ready(function () {
 					// subtotal, tax and grand total
 					applyCartAmounts(
 						response.cart_amount["subtotal"],
-						response.cart_amount["tax"],
+						response.cart_amount["tax_dict"],
 						response.cart_amount["grand_total"]
 					);
 				}
@@ -134,7 +134,7 @@ $(document).ready(function () {
 					// subtotal, tax and grand total
 					applyCartAmounts(
 						response.cart_amount["subtotal"],
-						response.cart_amount["tax"],
+						response.cart_amount["tax_dict"],
 						response.cart_amount["grand_total"]
 					);
 
@@ -169,7 +169,7 @@ $(document).ready(function () {
 					// subtotal, tax and grand total
 					applyCartAmounts(
 						response.cart_amount["subtotal"],
-						response.cart_amount["tax"],
+						response.cart_amount["tax_dict"],
 						response.cart_amount["grand_total"]
 					);
 
@@ -196,11 +196,16 @@ $(document).ready(function () {
 	}
 
 	// apply cart amounts
-	function applyCartAmounts(subtotal, tax, grand_total) {
+	function applyCartAmounts(subtotal, tax_dict, grand_total) {
 		if (window.location.pathname == "/cart/") {
 			$("#subtotal").html(subtotal);
-			$("#tax").html(tax);
 			$("#total").html(grand_total);
+
+            for(key1 in tax_dict) {
+                for(key2 in tax_dict[key1]){
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
 		}
 	}
     // ADD OPENING HOUR
