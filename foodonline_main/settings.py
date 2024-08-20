f387445fc20220835eac502e155836a6e6747362
Vinh Tong <tonggiavinh2010@gmail.com>
2024-08-20 11:28:58 +0700

    Load cover and profile picture

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'account.context_processors.get_google_api',
                 'marketplace.context_processors.get_cart_counter',
                 'marketplace.context_processors.get_cart_amounts',
+                'account.context_processors.get_user_profile',
             ],
         },
     },
