e852b7529de8a1d9bcee770989a250f799242216
Vinh Tong <tonggiavinh2010@gmail.com>
2024-08-20 16:44:44 +0700

    Place order views

--- foodonline_main/urls.py
+++ foodonline_main/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+import orders
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -33,5 +35,8 @@ urlpatterns = [
     
     # Checkout
     path('checkout/', MarketplaceViews.checkout, name='checkout'),
+    
+    # ORDERS
+    path('orders/', include('orders.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
