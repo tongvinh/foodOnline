0c5382b6c53f3c55f72defbb77fb2b4d1266201b
Vinh Tong <tonggiavinh2010@gmail.com>
2024-08-20 15:10:43 +0700

    Prepopulate Builling form

--- marketplace/views.py
+++ marketplace/views.py
@@ -2,6 +2,7 @@ from datetime import date, datetime
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
+from account.models import UserProfile
 from marketplace.context_processors import get_cart_amounts, get_cart_counter
 from marketplace.models import Cart
 from menu.models import Category, FoodItem
@@ -158,10 +159,29 @@ def search(request):
         
         return render(request, 'marketplace/listings.html', context=context)
     
-    
+
+@login_required(login_url='login')    
 def checkout(request):
-    form = OrderForm()
+    cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('marketplace')
+    
+    user_profile = UserProfile.objects.get(user=request.user)
+    default_values = {
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+        'phone': request.user.phone_number,
+        'email': request.user.email,
+        'address': user_profile.address,
+        'country': user_profile.country,
+        'state': user_profile.state,
+        'city': user_profile.city,
+        'pin_code': user_profile.pin_code
+    } 
+    form = OrderForm(initial=default_values)
     context = {
-        'form': form
+        'form': form,
+        'cart_items': cart_items
     }
     return render(request, 'marketplace/checkout.html', context=context)
