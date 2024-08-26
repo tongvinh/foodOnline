a5c495f7d1fd259cd984396966e1bebd5a332fb7
Vinh Tong <tonggiavinh2010@gmail.com>
2024-08-26 15:20:23 +0700

    Calculate total revenue and current month revenue

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import redirect, render
 
 from orders.models import Order
@@ -182,10 +183,25 @@ def vendorDashboard(request):
     vendor = Vendor.objects.get(user=request.user)
     orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
     recent_orders = orders[:10]
+    
+    # current month's revenue
+    current_month = datetime.datetime.now().month
+    current_month_orders = orders.filter(vendors__in=[vendor.id], created_at__month=current_month)
+    current_month_revenue = 0
+    for i in current_month_orders:
+        current_month_revenue += i.get_total_by_vendor()['grand_total']
+    
+    
+    # total revenue
+    total_revenue = 0
+    for i in orders:
+        total_revenue += i.get_total_by_vendor()['grand_total']
     context = {
         'orders': orders,
         'orders_count': orders.count(),
         'recent_orders': recent_orders,
+        'total_revenue': total_revenue,
+        'current_month_revenue': current_month_revenue
     }
     return render(request, 'accounts/vendorDashboard.html', context)
 
